0f78e5f51e11318c14c552f70ec94bbe10b368b5
idosdool <53960642+idosdool@users.noreply.github.com>
2022-09-18 19:56:19 +0100

    install service done properly

--- tinky.cpp
+++ tinky.cpp
@@ -53,6 +53,8 @@ int usage(int ac, char **av)
 
 int assign_scmanager()
 {
+	int err;
+
 	scManager = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
 	if (!scManager)
 	{
@@ -66,13 +68,55 @@ int assign_scmanager()
 	return (0);
 }
 
+// Create and install service tinky --- 
+
+int install(char *path)
+{
+	SC_HANDLE scService = OpenService(scManager, SVCNAME, SERVICE_ALL_ACCESS);
+	if (scService)
+	{
+		printf("Service %s exists.\n", SVCNAME);
+		return (1);
+	}
+	scService = CreateService(scManager,SVCNAME, SVCNAME,
+							SERVICE_ALL_ACCESS, SERVICE_WIN32_OWN_PROCESS,
+							SERVICE_DEMAND_START, SERVICE_ERROR_NORMAL,
+							path, NULL, NULL, NULL, NULL, NULL);
+	if (!scService)
+	{
+		printf("CreateService failed error (%ld)\n", GetLastError());
+		return (1);
+	}
+	printf("Service %s installed successfully.\n", SVCNAME);
+	return (0);
+}
+
+void do_op(int op)
+{
+	WCHAR		full_path[MAX_PATH];
+	char		path[MAX_PATH];
+	size_t		n = 0;
+
+	GetModuleFileNameW(NULL, full_path, MAX_PATH);
+	wcstombs_s(&n, path, MAX_PATH, full_path, wcslen(full_path));
+	if (op == 1)
+	{
+		install(path);
+	}
+	else
+		printf("smtg else\n");
+}
+
 int main(int ac, char **av)
 {
-	long int err;
-	if (usage(ac, av))
+	long int	err;
+	int			op;
+
+	if ((op = usage(ac, av)))
 	{
 		if (assign_scmanager())
 			return (1);
+		do_op(op);
 		if (!CloseServiceHandle(scManager))
 		{
 			printf("CloseServiceHandle failed error (%ld)\n", GetLastError());
